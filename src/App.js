c969087f4f59f5874582161472e3ea8c823ef299
satish <rahul>
2023-05-31 13:10:05 +0530

    us

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from './pages/Home';
 import { Provider } from 'react-redux';
 import store from './store/Store'
 import Footer from './components/Footer';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
 
 function App() {
   return (
